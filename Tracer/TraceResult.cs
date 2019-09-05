618ca22722b45d030c2e9ead40c74bb228c16dc4
vovamisjul <vovamisjul@gmail.com>
2019-09-05 20:52:22 +0300

    add trace result

---
+++ Tracer/TraceResult.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+
+namespace Tracer
+
+{
+    class TraceResult
+    {
+        public List<MeasuredThread> threads = new List<MeasuredThread>();
+
+        public void AddNewThread(MethodBase method)
+        {
+            MeasuredThread thread = new MeasuredThread(method);
+
+        }
+    }
+
+    class MeasuredMethod
+    {
+        public string MethodName { get; set; }
+        public string ClassName { get; set; }
+        public long Time { get; set; }
+        public List<MeasuredMethod> Methods { get; set; }
+        private Stopwatch Watch { get; set; } = new Stopwatch();
+
+        public MeasuredMethod(MethodBase method)
+        {
+            MethodName = method.Name;
+            ClassName = method.DeclaringType.Name;
+            Watch.Start();
+        }
+
+        public void StopMeasure()
+        {
+            Watch.Stop();
+            Time = Watch.ElapsedMilliseconds;
+        }
+    }
+
+    class MeasuredThread
+    {
+        public int Id { set; get; }
+        public long Time { set; get; }
+        public List<MeasuredMethod> methods { set; get; }
+
+        public MeasuredThread(MethodBase method)
+        {
+            Id = Thread.CurrentThread.ManagedThreadId;
+            methods.Add(new MeasuredMethod(method));
+        }
+    }
+
+
+}
---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer.Tracer
+{
+    class Tracer : ITracer
+    {
+        TraceResult traceResult = new TraceResult();
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+
+        public void StartTrace()
+        {
+            var stackFrame = new StackFrame(1);
+            stackFrame.GetMethod();
+            if (traceResult.threads.Any(elem => elem.Id == Thread.CurrentThread.ManagedThreadId))
+                traceResult.threads
+
+
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
